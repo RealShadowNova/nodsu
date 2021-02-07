e1d73bbe2e61cbf687b3205979b5c4ba08f1c1e0
Ethan Mitchell <ethamitc@gmail.com>
2021-02-06 22:26:32 -0500

    Add typedoc comments

--- src/Client.ts
+++ src/Client.ts
@@ -2,15 +2,23 @@ import { Requester } from './Requester';
 import { UserComponent } from './components/User';
 
 export class Client {
+  /** @internal */
   private readonly accessToken?: string;
-  readonly requester: Requester;
-
+  /** @internal */
+  private readonly requester: Requester;
+  /**
+   * User based API methods.
+   */
   public user: UserComponent;
 
+  /**
+   * 
+   * @param accessToken Default access token, recommended use is for client authorization.
+   */
   constructor(accessToken?: string) {
     this.accessToken = accessToken;
     this.requester = new Requester(this.accessToken);
-
+    
     this.user = new UserComponent(this.requester);
   }
 }
