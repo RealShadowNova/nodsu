92d272c1ff15458d9261826ecfbbe2b7ef42c08e
Ethan Mitchell <ethamitc@gmail.com>
2021-02-06 22:26:22 -0500

    Add typedoc comments

--- src/components/User.ts
+++ src/components/User.ts
@@ -2,12 +2,22 @@ import { Requester } from '../Requester';
 import { GameMode, User } from '../Types';
 
 export class UserComponent {
+  /** @internal */
   private readonly api: Requester;
 
   constructor(api: Requester) {
     this.api = api;
   }
 
+  /**
+   * Get Self Data
+   * 
+   * ```
+   * Client.user.getSelf('myAccessToken', 'osu');
+   * ```
+   * @param accessToken The oAuth access token (Cannot be a client grant token).
+   * @param mode 
+   */
   async getSelf(accessToken: string, mode?: GameMode): Promise<User> {
     let data: User = await this.api.get(`/me${mode ? `/${mode}` : ''}`, accessToken);
 
