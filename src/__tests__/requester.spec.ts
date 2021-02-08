b18bdb531f355b7488e85772d019e76da1157459
Hezekiah Hendry <46537907+RealShadowNova@users.noreply.github.com>
2021-02-07 22:51:05 -0500

    refactor: 💡 structure design

--- src/__tests__/requester.spec.ts
+++
@@ -1,9 +0,0 @@
-import { Requester } from '../Requester';
-
-describe('class Requester', () => {
-  test('Invalid JSON response', async () => {
-    const requester = new Requester();
-
-    await expect(requester.get('/this/does/not/exist', ''))
-  });
-})
